e6bef50f6c70d60b9cc9ef9f7ef5920a729407d9
mushfiqueghayas <mushfiqueshaikh69@gmail.com>
2025-05-03 16:26:37 +0000

    added

---
+++ kubernetes/deploy.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+cd /home/mushfique/opentelemetry-Ecommerce/kubernetes/loadgenerator
+vim deploy.yaml
+vim svc.yaml
+
+cd /home/mushfique/opentelemetry-Ecommerce/kubernetes/payment
+vim deploy.yaml
+vim svc.yaml
+
+cd /home/mushfique/opentelemetry-Ecommerce/kubernetes/productcatalog
+vim deploy.yaml
+vim svc.yaml
+
+cd /home/mushfique/opentelemetry-Ecommerce/kubernetes/quote
+vim deploy.yaml
+vim svc.yaml
+
+cd /home/mushfique/opentelemetry-Ecommerce/kubernetes/recommendation
+vim deploy.yaml
+vim svc.yaml
+
+
+cd /home/mushfique/opentelemetry-Ecommerce/kubernetes/shipping
+vim deploy.yaml
+vim svc.yaml
+
+
+cd /home/mushfique/opentelemetry-Ecommerce/kubernetes/valkey
+vim deploy.yaml
+vim svc.yaml
